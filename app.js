082c942ca4d809b32deced109d57b2e4eb38ae95
Anh Trieu <anh.trieu@icloud.com>
2018-01-12 10:49:14 -0800

    finished

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+app.disable('x-powered-by')
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.get('/ping', (req, res, next) => {
+  res.status(200).json('pong!')
+})
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json( { error: err } )
+})
+
+app.use((req, res, next) => {
+  res.status(404).json( { error: { message: `Not Found.` } } )
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
